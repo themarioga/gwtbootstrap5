5f5e01ae3e9a0b4ac426f17890acdd894a606f95
Mario Gonzalez Alvarado <marioga2512@gmail.com>
2023-12-18 18:11:43 +0100

    Updated to gwtbootstrap4

--- gwtbootstrap4/src/main/java/org/gwtbootstrap4/client/ui/SuggestBox.java
+++ gwtbootstrap4/src/main/java/org/gwtbootstrap4/client/ui/SuggestBox.java
@@ -9,9 +9,9 @@ package org.gwtbootstrap4.client.ui;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -128,7 +128,7 @@ public class SuggestBox extends com.google.gwt.user.client.ui.SuggestBox impleme
             resizePopup(suggestBox);
             if (popupResizeHandler == null) {
                 popupResizeHandler = new ResizeHandler() {
-                    private Timer timer = new Timer() {
+                    private final Timer timer = new Timer() {
                         public void run() {
                             resizePopup(suggestBox);
                         }
